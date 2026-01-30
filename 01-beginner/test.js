5b690327295009a80fc92323f7762d210302acff
JekenMaharjan <maharjanjeken@gmail.com>
2026-01-30 23:03:20 +0545

    Complete Beginner Exam 1: JS Basics

---
+++ 01-beginner/test.js
@@ -0,0 +1,94 @@
+
+// PART B – PRACTICAL TEST
+
+// Q6.Variable Test
+
+// Declare variables using let and const
+// name → your name
+// age → your age
+// isStudent → true or false
+const Name = "Jeken Maharjan";
+let age = 25;
+let isStudent = true;
+
+// Print them using console.log
+console.log(Name);
+console.log(age);
+console.log(isStudent);
+
+// -----------------------------------------
+
+// Q7. Data Type Check
+
+// Declare variables of type:
+// string, number, boolean, undefined, null
+const fullName = "Hari Maharjan";
+const rollNo = 45;
+const isEntered = false;
+let notDefine;
+const empty = null;
+
+// Use typeof to print each type
+console.log(typeof (fullName));
+console.log(typeof (rollNo));
+console.log(typeof (isEntered));
+console.log(typeof (notDefine));
+console.log(typeof (empty));
+
+// -----------------------------------------
+
+// Q8. Operator Test
+
+// Declare two numbers
+const a = 12;
+const b = 15;
+
+// Print:
+// sum
+const sum = (a + b);
+
+// difference
+const diff = (a - b);
+
+// product
+const prod = (a * b);
+
+// division
+const div = (a / b);
+
+console.log(sum);
+console.log(diff);
+console.log(prod);
+console.log(div);
+
+// -----------------------------------------
+
+// Q9. Condition Test
+
+// Write a program that checks:
+// if age is 18 or above → print "Eligible to vote"
+// else → print "Not eligible to vote"
+const ageOne = 24
+
+if (ageOne >= 18) {
+    console.log("Eligible to Vote!");
+} else {
+    console.log("Not eligible to Vote!");
+}
+
+// -----------------------------------------
+
+// Q10.Equality Test
+
+// Compare these two values:
+// 10 and "10"
+// Print result using ==
+// Print result using ===
+const x = 10;
+const y = "10";
+
+if (x == y) {
+    console.log("10 == '10' is True");
+} else if (x === y) {
+    console.log("10 === '10' is True");
+}
---
+++ 01-beginner/theory.md
@@ -0,0 +1,37 @@
+
+# BEGINNER LEVEL – EXAM 1
+
+## Topic: JavaScript Basics (Very Important Foundations)
+
+## Syllabus
+-> This test checks:
+    1. Variables (var, let, const)
+    2. Data types
+    3. Operators
+    4. console.log
+    5. Simple conditions
+
+## PART A – THEORY
+
+Q1. What is JavaScript? Where does it run?
+-> JavaScript is programming language which is used for interactive components in building web applications like error handling in forms, display on console of web browser, etc.
+-> It runs on web browser.
+
+Q2. Difference between var, let, and const
+-> 'var' means variable whose value can be changed.
+-> 'let' means similar to the variable.
+-> 'const' means constant whose value is most likely always same.
+
+Q3. What are primitive data types in JavaScript?
+-> Primitive datatypes in JavaScript are:
+    1. string
+    2. integer/number
+    3. character (char)
+    4. boolean
+
+Q4. What does console.log() do and why is it important?
+-> It shows the output display(print) on the console of the web browser.
+-> It important because it help us test if the program in Javascript it working well or not and also helps in debugging.
+
+Q5. Difference between == and ===
+-> '==' means that is equal to this i.e. a = b without checking its datatypes whereas '===' is similar to '==' but checks its datatypes properly.
